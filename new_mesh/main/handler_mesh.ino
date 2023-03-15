f3bc5e9d9c6a6af1435951dd0b97bf2272fd2660
Dennis <dsvignacio@gmail.com>
2023-03-15 18:07:53 +0800

    Working handler_mesh

--- new_mesh/main/handler_mesh.ino
+++ new_mesh/main/handler_mesh.ino
@@ -17,7 +17,7 @@ Change node number for each node
 TODO: visually label each node
 ####################################################################
 */
-#define NODE_NUMBER 1
+#define NODE_NUMBER 2
 
 // Run tasks in the background
 Scheduler userScheduler;
@@ -30,8 +30,8 @@ Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
 // 
 void sendMessage() {
   String hi = "Hi from Node ";
-  String msg = hi + NODE_NUMBER + "::";
-  msg += mesh.getNodeId();
+  String msg = hi + NODE_NUMBER;
+  // msg += mesh.getNodeId();
   mesh.sendBroadcast(msg);
   taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
 }
@@ -56,10 +56,20 @@ void nodeTimeAdjustedCallback(int32_t offset) {
   Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
 }
 
-// void setup() {
+void setup_mesh() {
+  // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
-// }
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-// void loop() {
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+}
 
-// }
+void loop_mesh() {
+  mesh.update();
+}
--- new_mesh/main/main.ino
+++ new_mesh/main/main.ino
@@ -1,21 +1,8 @@
 void setup() {
-  // 115200
-  Serial.begin(9600);
-
-  // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
-
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-
-  userScheduler.addTask( taskSendMessage );
-  taskSendMessage.enable();
+  Serial.begin(115200);
+  setup_mesh();
 }
 
 void loop() {
-  // it will run the user scheduler as well
-  mesh.update();
+  loop_mesh();
 }
