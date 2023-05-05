6e01a702362fd41c27bf88b31fd820dc43a96abb
Dennis Ignacio <dsvignacio@gmail.com>
2023-05-05 16:26:34 +0800

    Add packet loss feature; find the units for CO, CO2, PM2.5, and PM10

--- main_gateway_router/X_receive_JSON.ino
+++ main_gateway_router/X_receive_JSON.ino
@@ -1,3 +1,5 @@
+#include <ArduinoJson.h>
+
 void setup_receive_JSON()
 {
   
@@ -16,7 +18,7 @@ void loop_receive_JSON()
 
     if (err == DeserializationError::Ok)
     {
-      int node = doc["node"];
+      uint32_t node = doc["node"];
       double tempC = doc["tempC"];
       double tempF = doc["tempF"];
       double hum = doc["hum"];
@@ -24,34 +26,60 @@ void loop_receive_JSON()
       double CO2 = doc["CO2"];
       double pm25 = doc["PM 2.5"];
       double pm10 = doc["PM 10"];
+      uint32_t msg_sent_success = doc["msg_success"].as<uint32_t>();
+      uint32_t msg_sent_fail = doc["msg_fail"].as<uint32_t>();
 
-      if (node != 0) { 
-        Serial.printf("Node: %i \n", doc["node"].as<int>()); 
+      if (node > 0) { 
+        Serial.printf("==========\nNode: %u \n", doc["node"].as<uint32_t>()); 
       }
-      if ((tempC != 0) && (tempF != 0)) { 
-        Serial.printf("Temperature: %f C; %f F \n", doc["tempC"].as<double>(), doc["tempF"].as<double>()); 
+      if (msg_sent_success > 0) { 
+        Serial.printf("Success: %u \n", doc["msg_success"].as<uint32_t>()); 
       }
-      if (hum != 0) { 
-        Serial.printf("Humidity: %f %% \n", doc["hum"].as<double>()); 
+      if (msg_sent_fail > 0) { 
+        Serial.printf("Fail: %u \n", doc["msg_fail"].as<uint32_t>()); 
       }
-      if (CO != 0) { 
-        Serial.printf("CO: %f INSERT_UNITS_HERE \n", doc["CO"].as<double>()); 
+
+      double total_success_fail = msg_sent_success + msg_sent_fail;
+      double packet_loss = msg_sent_fail / total_success_fail;
+      Serial.printf("Packet loss: %lf%% \n", packet_loss*100); 
+
+      /* 
+      #######################################################
+        Sensors
+      #######################################################
+      */
+
+      if ((tempC > 0) && (tempF > 0)) { 
+        Serial.printf("Temperature: %lf C; %lf F \n", doc["tempC"].as<double>(), doc["tempF"].as<double>()); 
       }
-      if (CO2 != 0) { 
-        Serial.printf("CO2: %f INSERT_UNITS_HERE \n", doc["CO2"].as<double>());
+      if (hum > 0) { 
+        Serial.printf("Humidity: %lf %% \n", doc["hum"].as<double>()); 
       }
-      if (pm25 != 0) { 
-        Serial.printf("PM 2.5: %f ug/m^3 \n", pm25); 
+
+      /* 
+      #######################################################
+        Identify the units of CO, CO2, PM2.5, and PM10
+      #######################################################
+      */
+
+      if (CO > 0) { 
+        Serial.printf("CO: %lf INSERT_UNITS_HERE \n", doc["CO"].as<double>()); 
       }
-      if (pm10 != 0) { 
-        Serial.printf("PM 10: %f ug/m^3 \n", pm10); 
+      if (CO2 > 0) { 
+        Serial.printf("CO2: %lf INSERT_UNITS_HERE \n", doc["CO2"].as<double>());
+      }
+      if (pm25 > 0) { 
+        Serial.printf("PM 2.5: %lf INSERT_UNITS_HERE \n", pm25); 
+      }
+      if (pm10 > 0) { 
+        Serial.printf("PM 10: %lf INSERT_UNITS_HERE \n", pm10); 
       }
     } 
     else 
     {
       // Print error to the "debug" serial port
-      Serial.print("deserializeJson() returned ");
-      Serial.println(err.c_str());
+      // Serial.print("deserializeJson() returned ");
+      // Serial.println(err.c_str());
   
       // Flush all bytes in the "link" serial port buffer
       while (Serial2.available() > 0)
