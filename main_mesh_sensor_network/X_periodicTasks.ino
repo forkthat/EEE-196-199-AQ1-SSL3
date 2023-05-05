d3a9e22a0e8135631330f14f519d195ee8fbee7f
Dennis Ignacio <dsvignacio@gmail.com>
2023-05-05 16:27:57 +0800

    Randomize sending packets to decrease overhead/pressure on gateway

--- main_mesh_sensor_network/X_periodicTasks.ino
+++ main_mesh_sensor_network/X_periodicTasks.ino
@@ -12,30 +12,30 @@ Task taskSendMessage_SDS011(taskSendMsg_SDS011_seconds, TASK_FOREVER, &SendMessa
   ####################################################################
 */
 
-uint32_t gateway_mesh_ID = 1843304141;
-
 void sendToGateway(uint32_t gateway_mesh_ID, String &msg){
   if(mesh.sendSingle(gateway_mesh_ID, msg)){
     Serial.printf("Sending success to %u msg=%s\n", gateway_mesh_ID, msg.c_str());
+    msg_sent_success++;
   } else {
     Serial.printf("Sending failed to %u msg=%s\n", gateway_mesh_ID, msg.c_str());
+    msg_sent_fail++;
   }
 }
 
 void sendMessage_DHT22(){
   String msg = getReadings_DHT22();
   sendToGateway(gateway_mesh_ID, msg);
-  taskSendMessage_DHT22.setInterval(taskSendMsg_DHT22_seconds);
+  taskSendMessage_DHT22.setInterval(random(taskSendMsg_DHT22_seconds_low, taskSendMsg_DHT22_seconds_high));
 }
 
 void sendMessage_MQ135(){
   String msg = getReadings_MQ135();
   sendToGateway(gateway_mesh_ID, msg);
-  taskSendMessage_MQ135.setInterval(taskSendMsg_MQ135_seconds);
+  taskSendMessage_MQ135.setInterval(random(taskSendMsg_MQ135_seconds_low, taskSendMsg_MQ135_seconds_high));
 }
 
 void SendMessage_SDS011(){
   String msg = getReadings_SDS011();
   sendToGateway(gateway_mesh_ID, msg);
-  taskSendMessage_SDS011.setInterval(taskSendMsg_SDS011_seconds);
+  taskSendMessage_SDS011.setInterval(random(taskSendMsg_SDS011_seconds_low, taskSendMsg_SDS011_seconds_high));
 }
