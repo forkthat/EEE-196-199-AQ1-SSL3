df2b2f8a3d328542fe28f5b2eed1ac411d463317
PiLord1 <51207250+PiLord1@users.noreply.github.com>
2023-06-30 23:34:51 +0800

    Add files via upload
    Throughput, latency, and packet loss calculations for star topology

---
+++ ALL CODE/star_topology/Throughput, Latency, PL/Clients/node 2/node 2.ino
@@ -0,0 +1,123 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+#include <ThingsBoard.h>
+
+#define TOKEN "NODE1_TOKEN" // change this kasi ibang dashboard per node
+
+// const char* ssid = "Experimental Network";
+// const char* password = "tanongmokayjaybie";
+// const char* serverIP = "10.158.66.45";
+
+// const char* ssid = "PLDTHOMEFIBR648f8";
+// const char* password = "PLDTWIFIxvxz3";
+// const char* serverIP = "192.168.1.16";
+// const int serverPort = 1234;
+
+const char* ssid = "PLDTHOMEFIBRwKJ3r";
+const char* password = "PLDTWIFIjwq5R";
+const char* serverIP = "192.168.1.72";
+const int serverPort = 1234;
+
+WiFiClient wifiClient;
+
+char thingsboardServer[] = "thingsboard.cloud";
+ThingsBoard tb(wifiClient);
+
+const int packetSize = 1024; // each packet has 1024 bytes
+const int numPackets = 12;  // Total number of packets to send 
+const int totalSize = packetSize * numPackets;  // Total data size in bytes (12 kb) 
+
+WiFiUDP udpClient;
+IPAddress serverAddress;
+unsigned int packetCount = 0;
+unsigned long startTime = 0;
+unsigned long endTime = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Print the assigned IP address
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  serverAddress = IPAddress();
+  if (!serverAddress.fromString(serverIP)) {
+    Serial.println("Invalid server IP address");
+    while (1) {
+      delay(1000);
+    }
+  }
+
+  udpClient.begin(1234);  // UDP port for client
+}
+
+void loop() {
+  if (packetCount == 0) {
+    // Start measuring
+    startTime = millis();
+  }
+
+  // Prepare packet with dummy data
+  byte packet[packetSize];
+  for (int i = 0; i < packetSize; i++) {
+    packet[i] = random(255);
+  }
+
+  // Send packet to server
+  udpClient.beginPacket(serverAddress, serverPort);
+  udpClient.write(packet, packetSize);
+  udpClient.endPacket();
+
+  packetCount++;
+
+  if (packetCount == numPackets) {
+    // Finished sending packets, stop measuring
+    endTime = millis();
+    float elapsedTime = endTime - startTime;  // milliseconds
+    float throughput = (totalSize * 8) / (elapsedTime / 1000.0) / 1000.0;  // Mbps
+    float averageLatency = elapsedTime / numPackets;  // milliseconds
+    float packetLoss = ((numPackets - packetCount) / static_cast<float>(numPackets)) * 100.0;
+
+    Serial.println("Measurement Results:");
+    Serial.print("Throughput: ");
+    Serial.print(throughput);
+    Serial.println(" Mbps");
+    Serial.print("Average Latency: ");
+    Serial.print(averageLatency);
+    Serial.println(" ms");
+    Serial.print("Packet Loss: ");
+    Serial.print(packetLoss);
+    Serial.println("%");
+    if (!tb.connected()) {
+    while (!tb.connected()) {
+      Serial.println("Connecting to ThingsBoard node ...");
+      if (tb.connect(thingsboardServer, TOKEN)) {
+        Serial.println("Successfuly connected to ThingsBoard!");
+        tb.sendTelemetryFloat("N2 Throughput", throughput);
+        tb.sendTelemetryFloat("N2 Latency", averageLatency);
+        tb.sendTelemetryFloat("N2 Packet Loss", packetLoss);
+      } else {
+        Serial.print("[FAILED]");
+        Serial.println(" : retrying in 5 seconds]");
+        // Wait 5 seconds before retrying
+        delay(5000);
+      }
+    }
+  }
+
+    while (1) {
+      delay(1000);
+    }
+  }
+
+  // Wait for 5 minutes before sending the next batch of packets
+  delay(5 * 60 * 1000);
+}
---
+++ ALL CODE/star_topology/Throughput, Latency, PL/README.txt
@@ -0,0 +1,6 @@
+- May client and server communication para ma-measure throughput, latency, and packet loss
+- Every 5 minutes, 1024 bytes of a packet (packetSize) is sent to the server. 
+    - 1024 bytes per packet kasi yun yung normal
+    - Every 5 min pag-send ng data kasi sinisimulate natin kung nag-memeasure sensors every 5 min
+- For the duration of testing, it will determined by the number of packets (numPackets). For 1 hour of testing, numPackets = 12 kasi 12 * 5 minutes is 1 hour.
+- Sa example na to, total size of data to be transmitted = packetSize * numPackets = 1024 bytes * 12 packets ≈ 12 kilobytes
