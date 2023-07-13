7857a2f78d7a3018e19b7a87ccef42fd5a43e092
Dennis Ignacio <dsvignacio@gmail.com>
2023-07-14 01:13:58 +0800

    Remove average latency

--- ALL CODE/mesh_topology/main_mesh_sensor_network/2_Param_Latency.ino
+++ ALL CODE/mesh_topology/main_mesh_sensor_network/2_Param_Latency.ino
@@ -1,21 +1,11 @@
 void receivedDelayCallback(uint32_t nodeId, int32_t delay){
-  Serial.printf("Latency from %i: %i", nodeId, delay);
-  latency_data[latency_index] = delay;
-  latency_index += 1;
-
-  if (latency_index >= 10){
-    for(int i = 0; i < 10; i++){
-      sum_Latency += latency_data[i];
-    }
-    ave_Latency = sum_Latency/10;
-    latency_index = 0;
-  }
+  Serial.printf("Latency from %zu: %zu \n", nodeId, delay);
 
   StaticJsonDocument<128> doc;
   doc["node"] = nodeNumber;
   doc["msg_s"] = msg_sent_success;
   doc["msg_f"] = msg_sent_fail;
-  doc["lat"] = ave_Latency;
+  doc["lat"] = delay/pow(10, 6);      // convert microseconds to seconds
   msg_Latency = doc.as<String>();
   
   sendToGateway(gateway_mesh_ID, msg_Latency);
